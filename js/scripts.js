cbd540edef223653eb8621bb964440f4f94986f5
LenaKuchko <kuchkoel@gmail.com>
2017-05-06 20:57:12 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var number = parseInt($("#number").val());
+
 var ones = [];
 var breakPoint = [[400, 500], [40, 50]];
 var romans = [["CM","D", "C"],["XC", "L", "X"]];
@@ -50,7 +50,36 @@ function getConvertedNumber(input, romans, breakPoint) {
 
 $(function(){
   $("#form").submit(function(event){
+    var number = parseInt($("#number").val());
     console.log(getConvertedNumber(928, romans, breakPoint));
     event.preventDefault();
   });
 });
+
+
+// var value = [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]]
+
+// var array = [];
+
+
+// function getInput(input) {
+//   if (input === 0) {
+//     return "null";
+//   }
+//   else if (input < 0) {
+//     return "We only recognize whole numbers."
+//   }
+//   else if (input > 3999) {
+//     return "Please enter a number between 0 and 4,000."
+//   }
+//   else if (input >= 1) {
+//     for(var index = 0; index <= value.length; index ++){
+//       if(input >= value[index][0]){
+//         array.push(value[index][1]);
+//         input -= value[index][0]
+//         getInput(input)
+//         return array.join("");
+//       }
+//     }
+//   }
+// }
